bf8e9bb62bdb2032f929669b94c4fdb09c007f2a
svm2231 <93777985+svm2231@users.noreply.github.com>
2022-11-13 05:51:24 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,7 @@
+let string = "";
+let buttons = document.querySelectorAll('#btn');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target)
+    })
+})
